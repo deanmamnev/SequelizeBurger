dc4a150350594c09b2c62642d8a4e3bbe27262a0
Dean Mamnev <deanmamnev@gmail.com>
2018-04-17 14:17:22 -0500

    i am the eggman

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Triple Baconator');
+INSERT INTO burgers (burger_name) VALUES ('The HEartstopper');
+INSERT INTO burgers (burger_name) VALUES ('BeAllEndAll');
+
+
